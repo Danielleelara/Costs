1045c32ed14edbbd18f05de9309eed22a5f449f0
Danielle Souza <danielle.andrade@meta.com.br>
2022-01-25 19:54:48 -0300

    feat: criando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Components/Pages/Home';
+import Contact from './Components/Pages/Contact';
+import Company from './Components/Pages/Company';
+import NewProject from './Components/Pages/NewProject';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+        <Route path='/' exact>
+          <Home/>
+        </Route>
+        <Route path='/company' exact>
+          <Company/>
+        </Route>
+        <Route path='/contact' >
+          <Contact/>
+        </Route>
+        <Route path='/newproject' >
+          <NewProject/>
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
