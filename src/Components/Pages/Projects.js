6495ac08d1b55b48cc037ff3b7488d99fac27dd2
Danielle Souza <danielle.andrade@meta.com.br>
2022-01-30 14:41:47 -0300

    feat: modificando a tela de projetos

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,6 +1,9 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import Container from '../layout/Container'
 
+import styles from './Projects.module.css'
+import LinkButton from "../layout/LinkButton";
 const Projects = () => {
 
     const location = useLocation()
@@ -10,9 +13,15 @@ const Projects = () => {
     }
 
     return (  
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text= 'Criar Projeto'/> 
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass='start'>
+                <p>Projetos...</p>
+            </Container>
         </div>
      );
 }
